c06dbc6a0a0f407144dcaf7b28908466ce88b3ba
Dzuelu <24203504+Dzuelu@users.noreply.github.com>
2024-11-23 14:43:22 -0500

    Add react decoder for factorio blueprints

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { inflate } from "pako";
+
+export const initialExample =
+  "0eNqV0tEKgyAUBuB3OdcGy8wtX2WMUe0QB8xCbWyE7z7bYDeTmJeK/ye/nhU6veBsyXhQK1A/GQfqvIKjwbR62zPtiKBAtw5t4Rdr0UNgQOaGD1BluDBA48kTfpLvxfNqlrFDGw+wpMBgnlwMTWa7I0JFKSSDJyjJeQjsx+H/O2LPqf53+J4j8nuJlFN/nY6GAjX23lJfzJPG3XJJTOaXSzrH/HIy5ZzyPy3pNPm9ohMnkzyO29N+h5zBHa17J2rJG9E0tThW/FDVIbwAKnX+1A==";
+
+export const decodeBlueprint = (blueprintString: string): string => {
+  let decodedString = "Unable to parse blueprint";
+  try {
+    // Slice removes the version string at beginning of BP
+    const b64Decoded = window.atob(blueprintString.slice(1));
+    const decompressed = inflate(Uint8Array.from(b64Decoded, (c) => c.charCodeAt(0)));
+    const decoded = new TextDecoder("utf-8").decode(decompressed);
+    // pretty print json
+    decodedString = JSON.stringify(JSON.parse(decoded), undefined, 2);
+  } catch (error) {
+    console.error(error);
+  }
+  return decodedString;
+};
 
 function App() {
+  const [state, setState] = useState({
+    blueprintString: initialExample,
+    decodedString: decodeBlueprint(initialExample),
+  });
+
+  const onInputUpdated = (blueprintString: string): void =>
+    setState({
+      blueprintString,
+      decodedString: decodeBlueprint(blueprintString),
+    });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Input blueprint string
+        <br />
+        <input
+          value={state.blueprintString}
+          onInput={(e) => onInputUpdated((e.target as HTMLInputElement).value)}
+          onFocus={(e) => e.target.select()}
+        />
       </header>
+      <pre id="blueprint-contents">{state.decodedString.toString()}</pre>
     </div>
   );
 }
